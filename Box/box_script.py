2007531cb1de102b3aac230b251351f92f621b5d
VamshiPriyaVoruganti <vamshipriyav.sosio@gmail.com>
2017-04-26 17:17:58 +0530

    Box Files Added

---
+++ Box/box_script.py
@@ -0,0 +1,32 @@
+from __future__ import print_function, unicode_literals
+import os
+from boxsdk import Client
+from boxsdk.exception import BoxAPIException
+from boxsdk.object.collaboration import CollaborationRole
+from auth import authenticate
+
+
+def upload_download_file(client):
+    root_folder = client.folder(folder_id='0')
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'file.txt')
+    a_file = root_folder.upload(file_path, file_name='i-am-a-file.txt')
+    try:
+        print('{0} uploaded: '.format(a_file.get()['name']))
+        temp = str(client.file(file_id=a_file.get()['id']).content())
+        with open("Output.txt", "w") as text_file:
+            text_file.write(temp)
+    finally:
+        print('Done upload_download_file')
+
+def run_examples(oauth):
+    client = Client(oauth)
+    upload_download_file(client)
+    
+
+def main():
+    oauth, _, _ = authenticate()
+    run_examples(oauth)
+    os._exit(0)
+
+if __name__ == '__main__':
+    main()
